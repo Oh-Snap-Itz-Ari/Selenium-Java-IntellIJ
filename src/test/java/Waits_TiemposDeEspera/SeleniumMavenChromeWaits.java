e5f922c979a58cbc16a403f14f88dc8a29067db7
Mr. Blue Sky <alexfabianmelo12@hotmail.com>
2023-09-22 11:28:25 -0500

    Uso de Waits Selenium

---
+++ src/test/java/Waits_TiemposDeEspera/SeleniumMavenChromeWaits.java
@@ -0,0 +1,44 @@
+package Waits_TiemposDeEspera;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SeleniumMavenChromeWaits {
+    @Test
+    public void TestExecute() throws InterruptedException {
+
+        WebDriver driver;
+
+        System.setProperty("webdriver.chrome.driver", "D:\\Mr. Blue Sky\\Automatización\\Drivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.get("https://demoqa.com/automation-practice-form");
+
+        driver.findElement(By.id("firstName")).sendKeys("Alex Fabián");
+        driver.findElement(By.id("lastName")).sendKeys("Melo Gómez");
+        driver.findElement(By.id("userEmail")).sendKeys("alexfabianmelo12@hotmail.com");
+        driver.findElement(By.xpath("//label[@for='gender-radio-1'][contains(.,'Male')]")).click();
+        driver.findElement(By.id("userNumber")).sendKeys("3245447847");
+
+        Thread.sleep(3000);
+
+        WebElement El1 = driver.findElement(By.xpath("//label[@class='form-label'][contains(.,'Picture')]"));
+        WebElement El2 = driver.findElement(By.id("currentAddress"));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView();", El1); // Hace Scroll hasta que encuentra el El1
+
+        Thread.sleep(3000);
+
+        El2.sendKeys("Calle 30 # 05 - 15 San Antonio"); // Ingresa el contenido en la caja de texto del El2
+
+        Thread.sleep(5000);
+
+        driver.quit();
+    }
+}
